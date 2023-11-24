aa04d06cb0f912edf6726b794b1f24930b0ba997
unknown <jazbustos0@gmail.com>
2023-11-24 15:00:13 -0300

    script

--- js/script.js
+++ js/script.js
@@ -1,52 +1,48 @@
-//ENVIAR EMAIL A EMAILJS
-const btn = document.getElementById('button');
+// Agrega un controlador de eventos al formulario
+document.getElementById('form').addEventListener('submit', function(event) {
+  // Evita el comportamiento predeterminado de envío del formulario
+  event.preventDefault();
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+//Constantes
+  const nombreInput = document.getElementById('nombre');
+  const provinciaInput = document.getElementById('provincia');
+  const carreraInput = document.getElementById('carrera');
+  const telefonoInput = document.getElementById('telefono');
+  const emailInput = document.getElementById('email');
 
-   btn.value = 'Enviando...';
+  // Validar que no haya campos con espacios en blancos o vacíos
+  if (
+    nombreInput.value.trim() === "" ||
+    provinciaInput.value.trim() === "" ||
+    carreraInput.value.trim() === "" ||
+    telefonoInput.value.trim() === "" ||
+    emailInput.value.trim() === ""
+  ) {
+    alert("Por favor, complete todos los campos requeridos.");
+    return; // Detener la ejecución del resto del código
+  }
 
-   const serviceID = 'default_service';
-   const templateID = 'template_a5ey61k';
+  // Validar email
+  if (emailInput.validity.typeMismatch) {
+    emailInput.setCustomValidity("Introduzca una dirección de correo electrónico válida");
+    return; // Detener la ejecución del resto del código
+  } else {
+    emailInput.setCustomValidity("");
+  }
 
-   emailjs.sendForm(serviceID, templateID, this)
+  // Código de EmailJs para enviar formulario
+  const btn = document.getElementById('button');
+  btn.value = 'Enviando...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_a5ey61k';
+
+  emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Enviar';
-      alert('¡Gracias por contactarte con nosotros!\nPronto nos comunicaremos con vos.');
+      alert('¡Gracias por contactarte con nosotros!\nPronto nos comunicaremos contigo.');
     }, (err) => {
       btn.value = 'Enviar';
       alert(JSON.stringify(err));
     });
 });
-
-//VALIDACION DEL FORMULARIO
-/*
-function validarFormulario() {
-  var nombre = document.getElementById("nombre").value;
-  var provincia = document.getElementById("provincia").value;
-  var carrera = document.getElementById("carrera").value;
-  var telefono = document.getElementById("telefono").value;
-  var email = document.getElementById("email").value;
-
-  // Validar que los campos no estén vacíos
-  if (nombre === "" || provincia === "" || carrera === "" || telefono === "" || email === "") {
-    alert("Por favor, complete todos los campos requeridos.");
-    return false; // Detener el envío del formulario
-  }
-
-  // Validar el formato del correo electrónico (puedes usar una expresión regular)
-  var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-  if (!email.match(emailRegex)) {
-    alert("Por favor, ingrese una dirección de correo electrónico válida.");
-    return false;
-  }
-
-  // Validar otros campos según tus requerimientos
-  // Por ejemplo, puedes validar el formato del número de teléfono
-
-  // Si todo está validado, el formulario se enviará
-  return true;
-}
-*/
-
