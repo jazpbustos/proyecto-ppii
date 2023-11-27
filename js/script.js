0d696ac20baa9df9b36097f7f3d09c278625637b
unknown <jazbustos0@gmail.com>
2023-11-27 16:47:29 -0300

    prueba confirmada

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//FORMULARIO
+
 // Agrega un controlador de eventos al formulario
 document.getElementById('form').addEventListener('submit', function(event) {
   // Evita el comportamiento predeterminado de envío del formulario
@@ -45,4 +47,27 @@ document.getElementById('form').addEventListener('submit', function(event) {
       btn.value = 'Enviar';
       alert(JSON.stringify(err));
     });
+});
+
+//CARDS CARRUSEL
+var cardsSlider = new Swiper('.cards-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
